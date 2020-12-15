607e9c9656beeeee229b1d70190e76400050db8f
Sebastian Hernandez <shernandez@veritran.com>
2020-12-15 14:38:33 -0500

    Refactor robot test.

--- tests/test_robot.py
+++ tests/test_robot.py
@@ -5,13 +5,22 @@ from pyrobot.robot import PyRobot
 
 class PyRobotTest(TestCase):
 
-    def setUp(self) -> None:
+    def test_creates_instance_of_session(self):
+        self.given_a_brokers_auth_data()
+        self.when_a_robot_is_created()
+        self.the_robot_should_be_a_robot_instance()
+
+    def given_a_brokers_auth_data(self):
+        self.client_id = CLIENT_ID
+        self.redirect_uri = REDIRECT_URI
+        self.credentials_path = CREDENTIALS_PATH
+
+    def when_a_robot_is_created(self):
         self.robot = PyRobot(
-            client_id=CLIENT_ID,
-            redirect_uri=REDIRECT_URI,
-            credentials_path=CREDENTIALS_PATH
+            client_id=self.client_id,
+            redirect_uri=self.redirect_uri,
+            credentials_path=self.credentials_path
         )
 
-    def test_creates_instance_of_session(self):
+    def the_robot_should_be_a_robot_instance(self):
         self.assertIsInstance(self.robot, PyRobot)
-
